0e51930a00289dfe875a40d242b14e6cc82eb394
oyassine1911 <thvas1911@gmail.com>
2023-01-13 18:32:20 -0700

    updated package.json

--- server.js
+++ server.js
@@ -6,51 +6,55 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, 'Develop', 'public')));
+const dbPath = path.join(__dirname, 'Develop', 'db', 'db.json');
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Develop', 'public', 'favicon.ico'));
+});
 
 app.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
 });
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-fs.readFile(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8', (err, data) => {
-if (err) throw err;
-res.json(JSON.parse(data));
-});
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
 app.post('/api/notes', (req, res) => {
-fs.readFile(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8', (err, data) => {
-if (err) throw err;
-const notes = JSON.parse(data);
-const newNote = req.body;
-newNote.id = uuidv4();
-notes.push(newNote);
-fs.writeFile(path.join(__dirname, 'Develop', 'db', 'db.json'), JSON.stringify(notes), (err) => {
-if (err) throw err;
-res.json(newNote);
-});
-});
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-fs.readFile(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8', (err, data) => {
-if (err) throw err;
-let notes = JSON.parse(data);
-notes = notes.filter(note => note.id !== req.params.id);
-fs.writeFile(path.join(__dirname, 'Develop', 'db', 'db.json'), JSON.stringify(notes), (err) => {
-if (err) throw err;
-res.json(notes);
-});
-});
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(notes);
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
